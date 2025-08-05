d766ccb4e47154a4cf6679b2449f3c6b0027acf6
Victor Romano Arcuri <victor.r.arcuri@gmail.com>
2025-08-05 17:44:41 -0300

    feat: inicializa o app e o faz rodar na porta especificada

---
+++ index.ts
@@ -0,0 +1,8 @@
+import { app } from "./config/expressConfig";
+import dotenv from "dotenv";
+
+dotenv.config()
+
+app.listen(process.env.PORT, () => {
+    console.log(`Servidor hosteado na porta ${process.env.PORT}`)
+})
