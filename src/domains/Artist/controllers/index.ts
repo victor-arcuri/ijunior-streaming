8a535c34cefe50ff69eb71996e1130cd380c7de8
Victor Romano Arcuri <victor.r.arcuri@gmail.com>
2025-08-05 18:00:23 -0300

    feat: adiciona a base dos controllers de artista e música

---
+++ src/domains/Artist/controllers/index.ts
@@ -0,0 +1,6 @@
+import { Router, Request, Response, NextFunction } from "express";
+import serviceArtista from "../services/serviceArtista";
+
+const router = Router();
+
+export default router;
---
+++ src/domains/Music/controllers/index.ts
@@ -0,0 +1,6 @@
+import { Router, Request, Response, NextFunction } from "express";
+import serviceMusica from "../services/serviceMusica";
+
+const router = Router();
+
+export default router;
