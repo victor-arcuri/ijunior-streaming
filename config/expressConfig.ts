b51fd43ae67266bf7d207293c21d55bf2527f879
Victor Romano Arcuri <victor.r.arcuri@gmail.com>
2025-08-05 17:38:58 -0300

    feat: adiciona variável de ambiente da URL do app na config do Json

--- config/expressConfig.ts
+++ config/expressConfig.ts
@@ -1,9 +1,13 @@
 import express, { Express } from "express";
 import { CorsOptions } from "cors";
 
+import dotenv from "dotenv"
+
+dotenv.config()
+
 export const app:  Express = express();
 
 const options: CorsOptions = {
     credentials: true,
-    origin: "localhost:3000"
+    origin: process.env.APP_URL
 };
