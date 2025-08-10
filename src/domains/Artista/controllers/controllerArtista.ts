655cc550a7f4757e11305721b2d37395d0661479
Otavio L <220527245+otavio-l@users.noreply.github.com>
2025-08-10 20:00:12 -0300

    feat: adiciona "Listar artistas" endpoint

--- src/domains/Artista/controllers/controllerArtista.ts
+++ src/domains/Artista/controllers/controllerArtista.ts
@@ -1,6 +1,19 @@
 import { Router, Request, Response, NextFunction } from 'express';
 import serviceArtista from '../services/serviceArtista.js';
+import { success } from '../../../../config/statusCodes.js';
+
 
 const router = Router();
+
+router.get('/', async (req: Request, res: Response, next: NextFunction) => {
+    try {
+        const artistas = await serviceArtista.listarArtistas()
+        res.status(success.SUCCESS).json(artistas)
+    } catch (err) {
+        next(err)
+    }
+})
+
+
 
 export default router;
