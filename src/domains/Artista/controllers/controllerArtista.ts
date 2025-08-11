2d3fc8cadff558fc772716d5bdae287c8da41ed7
Otavio L <220527245+otavio-l@users.noreply.github.com>
2025-08-10 21:28:41 -0300

    refactor: prettier

--- src/domains/Artista/controllers/controllerArtista.ts
+++ src/domains/Artista/controllers/controllerArtista.ts
@@ -3,40 +3,39 @@ import { Prisma } from '@prisma/client';
 import serviceArtista from '../services/serviceArtista.js';
 import { success } from '../../../../config/statusCodes.js';
 
-
 const router = Router();
 
 router.get('/', async (req: Request, res: Response, next: NextFunction) => {
     try {
-        const artistas = await serviceArtista.listarArtistas()
-        res.status(success.SUCCESS).json(artistas)
+        const artistas = await serviceArtista.listarArtistas();
+        res.status(success.SUCCESS).json(artistas);
     } catch (err) {
-        next(err)
+        next(err);
     }
-})
+});
 
 router.post('/', async (req: Request, res: Response, next: NextFunction) => {
     try {
         const artista: Prisma.ArtistaCreateInput = {
             nome: req.body.nome,
             streams: req.body.streams,
-            foto: req.body.foto
-        }
-        const artistaCriado = await serviceArtista.criarArtista(artista)
-        res.status(success.CREATED).json(artistaCriado)
+            foto: req.body.foto,
+        };
+        const artistaCriado = await serviceArtista.criarArtista(artista);
+        res.status(success.CREATED).json(artistaCriado);
     } catch (err) {
-        next(err)
+        next(err);
     }
-})
+});
 
 router
     .route('/id/:id')
     .get(async (req: Request, res: Response, next: NextFunction) => {
         try {
-            const artista = await serviceArtista.listarArtistaID(req.params.id)
-            res.status(success.SUCCESS).json(artista)
+            const artista = await serviceArtista.listarArtistaID(req.params.id);
+            res.status(success.SUCCESS).json(artista);
         } catch (err) {
-            next(err)
+            next(err);
         }
     })
 
@@ -45,31 +44,31 @@ router
             const artista: Prisma.ArtistaUpdateInput = {
                 nome: req.body.nome,
                 streams: req.body.streams,
-                foto: req.body.foto
-            }
-            const artistaAtualizado = await serviceArtista.atualizaArtista(req.params.id, artista)
-            res.status(success.SUCCESS).json(artistaAtualizado)
+                foto: req.body.foto,
+            };
+            const artistaAtualizado = await serviceArtista.atualizaArtista(req.params.id, artista);
+            res.status(success.SUCCESS).json(artistaAtualizado);
         } catch (err) {
-            next(err)
+            next(err);
         }
     })
 
     .delete(async (req: Request, res: Response, next: NextFunction) => {
         try {
-            await serviceArtista.deletarArtista(req.params.id)
-            res.status(success.NO_CONTENT).send()
+            await serviceArtista.deletarArtista(req.params.id);
+            res.status(success.NO_CONTENT).send();
         } catch (err) {
-            next(err)
+            next(err);
         }
-    })
+    });
 
 router.get('/id/:id/musicas', async (req: Request, res: Response, next: NextFunction) => {
     try {
-        const musicas = await serviceArtista.listaMusicasArtista(req.params.id)
-        res.status(success.SUCCESS).json(musicas)
-    } catch(err) {
-        next(err)
+        const musicas = await serviceArtista.listaMusicasArtista(req.params.id);
+        res.status(success.SUCCESS).json(musicas);
+    } catch (err) {
+        next(err);
     }
-})
+});
 
 export default router;
