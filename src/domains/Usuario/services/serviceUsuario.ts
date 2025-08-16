c3461650138976a5629774d029a86192dd9b00ba
Otavio L <220527245+otavio-l@users.noreply.github.com>
2025-08-15 21:24:09 -0300

    refactor: adapta usuario services ao sistema de tratamento de erro

--- src/domains/Usuario/services/serviceUsuario.ts
+++ src/domains/Usuario/services/serviceUsuario.ts
@@ -1,8 +1,8 @@
 import prisma from '../../../../config/prismaClient.js';
 import bcrypt from 'bcrypt';
-import { PrismaClientKnownRequestError } from '@prisma/client/runtime/library';
 import { Prisma } from '@prisma/client';
 import { z } from 'zod';
+import { TokenError } from '../../../../errors/TokenError.js';
 
 const UsuarioCreate = z.object({
     email: z.email('Email inválido'),
@@ -31,242 +31,193 @@ const UsuarioUpdate = z
 class ServiceUsuario {
     // Cria novo usuário
     async criarUsuario(body: Prisma.UsuarioCreateInput) {
-        const hashSenha = await bcrypt.hash(body.senha, 10);
-        const usuario: Prisma.UsuarioCreateInput = {
-            nome: body.nome,
-            email: body.email,
-            senha: hashSenha,
-            privilegio: body.privilegio,
-            foto: body.foto,
-        };
+        const usuarioValido = UsuarioCreate.parse(body);
 
-        try {
-            const usuarioCriado = await prisma.usuario.create({
-                data: usuario,
-                omit: {
-                    senha: true,
-                },
-            });
+        usuarioValido.senha = await bcrypt.hash(usuarioValido.senha, 10);
 
-            return usuarioCriado;
-        } catch (erro) {
-            if (erro instanceof PrismaClientKnownRequestError) {
-                if (erro.code === 'P2002') {
-                    throw new Error('Erro ao criar usuário: e-mail já está em uso');
-                }
-            }
+        const usuarioCriado = await prisma.usuario.create({
+            data: usuarioValido,
+            omit: {
+                senha: true,
+            },
+        });
 
-            throw new Error(`Erro ao criar usuário: ${erro}`);
-        }
+        return usuarioCriado;
     }
 
     // Deleta usuário com id especifico
     async deletarUsuario(id: string) {
-        try {
-            await prisma.usuario.delete({
-                where: {
-                    id: id,
-                },
-            });
-        } catch (erro) {
-            throw new Error(`Erro ao deletar usuário: ${erro}`);
-        }
+        await prisma.usuario.delete({
+            where: {
+                id: id,
+            },
+        });
     }
 
     // Lista todos os usuários registrados
     async listarUsuarios(limit?: number, order?: Prisma.SortOrder) {
-        try {
-            const usuarios = await prisma.usuario.findMany({
-                orderBy: {
-                    id: order ?? 'asc',
-                },
-                omit: {
-                    senha: true,
-                },
-                take: typeof limit === 'number' ? limit : undefined,
-            });
-            return usuarios;
-        } catch (erro) {
-            throw new Error(`Erro ao listar usuários: ${erro}`);
-        }
+        const usuarios = await prisma.usuario.findMany({
+            orderBy: {
+                id: order ?? 'asc',
+            },
+            omit: {
+                senha: true,
+            },
+            take: typeof limit === 'number' ? limit : undefined,
+        });
+        return usuarios;
     }
 
     // Retoma o usuário com id especificado
     async listarUsuarioID(id: string) {
-        try {
-            const usuario = await prisma.usuario.findUnique({
-                where: {
-                    id: id,
-                },
-                omit: {
-                    senha: true,
-                },
-            });
-            return usuario;
-        } catch (erro) {
-            throw new Error(`Erro ao retomar usuário de id ${id}: ${erro}`);
+        const usuario = await prisma.usuario.findUnique({
+            where: {
+                id: id,
+            },
+            omit: {
+                senha: true,
+            },
+        });
+
+        if (!usuario) {
+            throw new TokenError('UUID v4 inválido');
         }
+
+        return usuario;
     }
 
     // Retoma o usuário com email especificado
     async listarUsuarioEmail(email: string) {
-        try {
-            const usuario = await prisma.usuario.findUnique({
-                where: {
-                    email: email,
-                },
-                omit: {
-                    senha: true,
-                },
-            });
-            return usuario;
-        } catch (erro) {
-            throw new Error(`Erro ao retomar usuário de email ${email}: ${erro}`);
+        const usuario = await prisma.usuario.findUnique({
+            where: {
+                email: email,
+            },
+            omit: {
+                senha: true,
+            },
+        });
+
+        if (!usuario) {
+            throw new TokenError('UUID v4 inválido');
         }
+
+        return usuario;
     }
 
     // Atualiza informações do usuário de id especificado
     async atualizaUsuario(id: string, body: Prisma.UsuarioUpdateInput) {
-        const usuarioUpdate: Prisma.UsuarioUpdateInput = { ...body };
-        if (typeof body.senha === 'string') {
-            usuarioUpdate.senha = await bcrypt.hash(body.senha, 10);
+        const usuarioUpdate = UsuarioUpdate.parse(body);
+        if (usuarioUpdate.senha) {
+            usuarioUpdate.senha = await bcrypt.hash(usuarioUpdate.senha, 10);
         }
-        try {
-            const usuario = await prisma.usuario.update({
-                where: {
-                    id: id,
-                },
-                omit: {
-                    senha: true,
-                },
-                data: usuarioUpdate,
-            });
-            return usuario;
-        } catch (erro) {
-            throw new Error(`Erro ao atualizar usuário de id ${id}: ${erro}`);
-        }
+        const usuario = await prisma.usuario.update({
+            where: {
+                id: id,
+            },
+            omit: {
+                senha: true,
+            },
+            data: usuarioUpdate,
+        });
+        return usuario;
     }
 
     async listaHistoricoUsuario(id: string) {
-        try {
-            const historico = await prisma.logMusica.findMany({
-                where: {
-                    usuarioId: id,
-                },
-                orderBy: {
-                    tempo: 'desc',
-                },
-                select: {
-                    tempo: true,
-                    musica: { select: { nome: true } },
-                    id: true,
-                },
-            });
-            return historico;
-        } catch (erro) {
-            throw new Error(`Erro ao listar histórico: ${erro}`);
-        }
+        const historico = await prisma.logMusica.findMany({
+            where: {
+                usuarioId: id,
+            },
+            orderBy: {
+                tempo: 'desc',
+            },
+            select: {
+                tempo: true,
+                musica: { select: { nome: true } },
+                id: true,
+            },
+        });
+        return historico;
     }
 
     async listaMusicasSalvasUsuario(id: string) {
-        try {
-            const musicasSalvas = await prisma.musicaSalva.findMany({
-                where: {
-                    usuarioId: id,
-                },
-                select: {
-                    musica: {
-                        select: {
-                            nome: true,
-                            autoria: {
-                                select: {
-                                    artista: {
-                                        select: {
-                                            nome: true,
-                                        },
+        const musicasSalvas = await prisma.musicaSalva.findMany({
+            where: {
+                usuarioId: id,
+            },
+            select: {
+                musica: {
+                    select: {
+                        nome: true,
+                        autoria: {
+                            select: {
+                                artista: {
+                                    select: {
+                                        nome: true,
                                     },
                                 },
                             },
-                            id: true,
                         },
+                        id: true,
                     },
                 },
-            });
-            return musicasSalvas;
-        } catch (erro) {
-            throw new Error(`Erro ao listar músicas salvas: ${erro}`);
-        }
+            },
+        });
+        return musicasSalvas;
     }
 
     async salvaMusicaUsuario(usuario_id: string, musica_id: string) {
-        try {
-            const musicaSalva = await prisma.musicaSalva.create({
-                data: {
-                    usuario: {
-                        connect: {
-                            id: usuario_id,
-                        },
-                    },
-                    musica: {
-                        connect: {
-                            id: musica_id,
-                        },
+        const musicaSalva = await prisma.musicaSalva.create({
+            data: {
+                usuario: {
+                    connect: {
+                        id: usuario_id,
                     },
                 },
-            });
-            return musicaSalva;
-        } catch (erro) {
-            throw new Error(`Erro ao salvar música: ${erro}`);
-        }
+                musica: {
+                    connect: {
+                        id: musica_id,
+                    },
+                },
+            },
+        });
+        return musicaSalva;
     }
 
     async removeMusicaSalvaUsuario(usuario_id: string, musica_id: string) {
-        try {
-            await prisma.musicaSalva.delete({
-                where: {
-                    usuarioId_musicaId: {
-                        usuarioId: usuario_id,
-                        musicaId: musica_id,
-                    },
+        await prisma.musicaSalva.delete({
+            where: {
+                usuarioId_musicaId: {
+                    usuarioId: usuario_id,
+                    musicaId: musica_id,
                 },
-            });
-        } catch (erro) {
-            throw new Error(`Erro ao remover música salva: ${erro}`);
-        }
+            },
+        });
     }
 
     async criaHistoricoUsuario(usuario_id: string, musica_id: string) {
-        try {
-            const logMusica = await prisma.logMusica.create({
-                data: {
-                    usuario: {
-                        connect: {
-                            id: usuario_id,
-                        },
-                    },
-                    musica: {
-                        connect: {
-                            id: musica_id,
-                        },
+        const logMusica = await prisma.logMusica.create({
+            data: {
+                usuario: {
+                    connect: {
+                        id: usuario_id,
                     },
                 },
-            });
-            return logMusica;
-        } catch (erro) {
-            throw new Error(`Erro ao criar log de música: ${erro}`);
-        }
+                musica: {
+                    connect: {
+                        id: musica_id,
+                    },
+                },
+            },
+        });
+        return logMusica;
     }
 
     async removeHistoricoUsuario(log_id: string) {
-        try {
-            await prisma.logMusica.delete({
-                where: {
-                    id: log_id,
-                },
-            });
-        } catch (erro) {
-            throw new Error(`Erro ao remover música do histórico: ${erro}`);
-        }
+        await prisma.logMusica.delete({
+            where: {
+                id: log_id,
+            },
+        });
     }
 }
 
