7bdb43e9542c36c9c3e12bb4891909b306df4596
Otavio L <220527245+otavio-l@users.noreply.github.com>
2025-08-21 18:31:23 -0300

    feat: adiciona dockerfile

---
+++ entrypoint.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+set -e
+
+# Wait for the DB to be ready
+until nc -z mysql-server 3306; do
+  echo "Waiting for MySQL..."
+  sleep 2
+done
+
+npx prisma migrate deploy
+
+exec node dist/index.js
