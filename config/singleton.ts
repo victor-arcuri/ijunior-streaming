0b9a1f1ac87c0c66fee634de4ce620d46f074c9e
Otavio L <220527245+otavio-l@users.noreply.github.com>
2025-08-28 10:54:08 -0300

    refactor: prettier

--- config/singleton.ts
+++ config/singleton.ts
@@ -1,14 +1,14 @@
-import { PrismaClient } from '@prisma/client'
-import { mockDeep, mockReset, DeepMockProxy } from 'jest-mock-extended'
-import prisma from './prismaClient.js'
+import { PrismaClient } from '@prisma/client';
+import { mockDeep, mockReset, DeepMockProxy } from 'jest-mock-extended';
+import prisma from './prismaClient.js';
 
 jest.mock('./prismaClient', () => ({
-  __esModule: true,
-  default: mockDeep<PrismaClient>(),
-}))
+    __esModule: true,
+    default: mockDeep<PrismaClient>(),
+}));
 
-export const prismaMock = prisma as unknown as DeepMockProxy<PrismaClient>
+export const prismaMock = prisma as unknown as DeepMockProxy<PrismaClient>;
 
 beforeEach(() => {
-  mockReset(prismaMock)
-})
+    mockReset(prismaMock);
+});
